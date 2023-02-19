e1195cf32599759a91cb132eba759e37e6fc51ee
fiacre <armeldion95@gmail.com>
2023-02-19 05:32:28 +0000

    first commit

---
+++ Daylichallenge.sql
@@ -0,0 +1,16 @@
+-- Database: Exercice4
+
+-- DROP DATABASE IF EXISTS "Exercice4";
+
+CREATE DATABASE "Exercice4"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'French_France.1252'
+    LC_CTYPE = 'French_France.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+	
+	SELECT INITCAP('Bonjour armel') FROM COMPANY;
+	
